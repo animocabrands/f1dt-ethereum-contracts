a99c2045689cb1a8f05c37f4cea9fcb9570a409a
Nathan Sala <sala.nathan@gmail.com>
2020-07-31 00:38:06 +0800

    prettification

--- contracts/token/ERC1155721/RepairCentre.sol
+++ contracts/token/ERC1155721/RepairCentre.sol
@@ -7,28 +7,11 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@animoca/ethereum-contracts-assets_inventory/contracts/token/ERC1155/ERC1155TokenReceiver.sol";
 
 contract NFTRepairCentre is Ownable, ERC1155TokenReceiver {
-
     using SafeMath for uint256;
 
-    event TokenRepaired (
-        uint256 defunctToken,
-        uint256 replacementToken
-    );
-
-    event TokensRepaired (
-        uint256[] defunctTokens,
-        uint256[] replacementTokens
-    );
-
-    event TokensToRepairAdded (
-        uint256[] brokenIds,
-        uint256[] fixedIds
-    );
-
-    struct TokenToRepair {
-        uint256 defunctToken;
-        uint256 replacementToken;
-    }
+    event TokensToRepairAdded(uint256[] brokenIds, uint256[] fixedIds);
+    event TokenRepaired(uint256 defunctToken, uint256 replacementToken);
+    event TokensRepaired(uint256[] defunctTokens, uint256[] replacementTokens);
 
     IDeltaTimeInventory inventoryContract;
     address tokensGraveyard;
@@ -37,84 +20,143 @@ contract NFTRepairCentre is Ownable, ERC1155TokenReceiver {
 
     mapping(uint256 => uint256) repairList;
 
-    constructor(address inventoryContract_, address tokensGraveyard_, address revvContract_, uint256 revvCompensation_) public {
-        require(inventoryContract_ != address(0) && tokensGraveyard_ != address(0) && revvContract_ != address(0), "NFTRepairCentre: zero address");
+    /**
+     * Constructor.
+     * @dev Reverts if one of the argument addresses is zero.
+     * @param inventoryContract_ the address of the DeltaTimeInventoryContract.
+     * @param tokensGraveyard_ the address of the tokens graveyard.
+     * @param revvContract_ the address of the REVV contract.
+     * @param revvCompensation_ the amount of REVV to compensate for each token replacement.
+     */
+    constructor(
+        address inventoryContract_,
+        address tokensGraveyard_,
+        address revvContract_,
+        uint256 revvCompensation_
+    ) public {
+        require(
+            inventoryContract_ != address(0) && tokensGraveyard_ != address(0) && revvContract_ != address(0),
+            "NFTRepairCentre: zero address"
+        );
         inventoryContract = IDeltaTimeInventory(inventoryContract_);
         tokensGraveyard = tokensGraveyard_;
         revvContract = IREVV(revvContract_);
         revvCompensation = revvCompensation_;
     }
 
-    function addTokensToRepair(uint256[] calldata brokenIds, uint256[] calldata fixedIds) external onlyOwner {
-        uint256 length = brokenIds.length;
-        require(length != 0 && length == fixedIds.length, "NFTRepairCentre: wrong arguments");
-        for (uint i = 0; i < length; ++i) {
-            repairList[brokenIds[i]] = fixedIds[i];
+    /**
+     * Adds tokens to the repair list. The necessary amount of REVV for the compensations is transferred to the contract.
+     * @dev Reverts if not called by the owner.
+     * @dev Reverts if `defunctTokens` and `replacementTokens` have inconsistent lengths.
+     * @dev Reverts ifthe REVV transfer fails.
+     * @dev Emits the TokensToRepairAdded event.
+     * @param defunctTokens the list of defunct tokens.
+     * @param replacementTokens the list of replacement tokens.
+     */
+    function addTokensToRepair(uint256[] calldata defunctTokens, uint256[] calldata replacementTokens)
+        external
+        onlyOwner
+    {
+        uint256 length = defunctTokens.length;
+        require(length != 0 && length == replacementTokens.length, "NFTRepairCentre: wrong arguments");
+        for (uint256 i = 0; i < length; ++i) {
+            repairList[defunctTokens[i]] = replacementTokens[i];
         }
         revvContract.transferFrom(msg.sender, address(this), revvCompensation.mul(length));
-        emit TokensToRepairAdded(brokenIds, fixedIds);
+        emit TokensToRepairAdded(defunctTokens, replacementTokens);
     }
 
     /*                                             ERC1155TokenReceiver                                             */
 
+    /**
+     * @notice Handle the receipt of a single ERC1155 token type.
+     * @dev An ERC1155-compliant smart contract MUST call this function on the token recipient contract, at the end of a `safeTransferFrom` after the balance has been updated.
+     * This function MUST return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))` (i.e. 0xf23a6e61) if it accepts the transfer.
+     * This function MUST revert if it rejects the transfer.
+     * Return of any other value than the prescribed keccak256 generated value MUST result in the transaction being reverted by the caller.
+     * @param /operator  The address which initiated the transfer (i.e. msg.sender)
+     * @param from      The address which previously owned the token
+     * @param id        The ID of the token being transferred
+     * @param value     The amount of tokens being transferred
+     * @param /data      Additional data with no specified format
+     * @return bytes4   `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
+     */
     function onERC1155Received(
         address, /*operator*/
         address from,
         uint256 id,
-        uint256, /*value*/
+        uint256 value,
         bytes calldata /*data*/
     ) external virtual override returns (bytes4) {
-        _repairToken(id, from);
+        _repairToken(from, id, value);
         return _ERC1155_RECEIVED;
     }
 
+    /**
+     * @notice Handle the receipt of multiple ERC1155 token types.
+     * @dev An ERC1155-compliant smart contract MUST call this function on the token recipient contract, at the end of a `safeBatchTransferFrom` after the balances have been updated.
+     * This function MUST return `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))` (i.e. 0xbc197c81) if it accepts the transfer(s).
+     * This function MUST revert if it rejects the transfer(s).
+     * Return of any other value than the prescribed keccak256 generated value MUST result in the transaction being reverted by the caller.
+     * @param /operator  The address which initiated the batch transfer (i.e. msg.sender)
+     * @param from      The address which previously owned the token
+     * @param ids       An array containing ids of each token being transferred (order and length must match _values array)
+     * @param values    An array containing amounts of each token being transferred (order and length must match _ids array)
+     * @param /data      Additional data with no specified format
+     * @return          `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
+     */
     function onERC1155BatchReceived(
         address, /*operator*/
         address from,
         uint256[] calldata ids,
-        uint256[] calldata, /*values*/
+        uint256[] calldata values,
         bytes calldata /*data*/
     ) external virtual override returns (bytes4) {
-        _repairTokens(ids, from);
+        _repairTokens(from, ids, values);
         return _ERC1155_BATCH_RECEIVED;
     }
 
-    function _repairToken(uint256 defunctToken, address from) internal {
+    function _repairToken(
+        address from,
+        uint256 defunctToken,
+        uint256 value
+    ) internal {
+        require(value == 1, "NFTRepairCentre: wrong value");
         uint256 replacementToken = repairList[defunctToken];
         require(replacementToken != 0, "NFTRepairCentre: wrong token");
         inventoryContract.safeTransferFrom(from, tokensGraveyard, defunctToken, 1, bytes(""));
-        try inventoryContract.mintNonFungible(from, replacementToken, bytes32(""), true) {
-        } catch {
+        try inventoryContract.mintNonFungible(from, replacementToken, bytes32(""), true)  {} catch {
             inventoryContract.mintNonFungible(from, replacementToken, bytes32(""), false);
         }
         revvContract.transfer(from, revvCompensation);
         emit TokenRepaired(defunctToken, replacementToken);
     }
 
-    function _repairTokens(uint256[] memory defunctTokens, address from) internal {
+    function _repairTokens(
+        address from,
+        uint256[] memory defunctTokens,
+        uint256[] memory values
+    ) internal {
         uint256 length = defunctTokens.length;
         require(length != 0, "NFTRepairCentre: wrong argument");
         address[] memory recipients = new address[](length);
         uint256[] memory replacementTokens = new uint256[](length);
         bytes32[] memory uris = new bytes32[](length);
-        uint256[] memory values = new uint256[](length);
         for (uint256 i = 0; i < length; ++i) {
+            require(values[i] == 1, "NFTRepairCentre: wrong value");
             uint256 replacementToken = repairList[defunctTokens[i]];
             require(replacementToken != 0, "NFTRepairCentre: wrong token");
             recipients[i] = from;
             replacementTokens[i] = replacementToken;
-            values[i] = 1;
         }
 
         inventoryContract.safeBatchTransferFrom(from, tokensGraveyard, defunctTokens, values, bytes(""));
-        try inventoryContract.batchMint(recipients, replacementTokens, uris, values, true) {
-        } catch {
+        try inventoryContract.batchMint(recipients, replacementTokens, uris, values, true)  {} catch {
             inventoryContract.batchMint(recipients, replacementTokens, uris, values, false);
         }
         revvContract.transfer(from, revvCompensation.mul(length));
         emit TokensRepaired(defunctTokens, replacementTokens);
     }
-
 }
 
 interface IDeltaTimeInventory {
@@ -131,7 +173,7 @@ interface IDeltaTimeInventory {
      * @param id      ID of the token type
      * @param value   Transfer amount
      * @param data    Additional data with no specified format, MUST be sent unaltered in call to `onERC1155Received` on `to`
-    */
+     */
     function safeTransferFrom(
         address from,
         address to,
@@ -155,7 +197,7 @@ interface IDeltaTimeInventory {
      * @param ids     IDs of each token type (order and length must match _values array)
      * @param values  Transfer amounts per token type (order and length must match _ids array)
      * @param data    Additional data with no specified format, MUST be sent unaltered in call to the `ERC1155TokenReceiver` hook(s) on `to`
-    */
+     */
     function safeBatchTransferFrom(
         address from,
         address to,
@@ -172,7 +214,13 @@ interface IDeltaTimeInventory {
      * @param uris bytes32[] Concatenated metadata URIs of nfts to be minted
      * @param values uint256[] List of quantities of ft to be minted
      */
-    function batchMint(address[] calldata to, uint256[] calldata ids, bytes32[] calldata uris, uint256[] calldata values, bool safe) external;
+    function batchMint(
+        address[] calldata to,
+        uint256[] calldata ids,
+        bytes32[] calldata uris,
+        uint256[] calldata values,
+        bool safe
+    ) external;
 
     /**
      * @dev Public function to mint one non fungible token id
@@ -181,7 +229,12 @@ interface IDeltaTimeInventory {
      * @param tokenId uint256 ID of the token to be minted
      * @param byteUri bytes32 Concatenated metadata URI of nft to be minted
      */
-    function mintNonFungible(address to, uint256 tokenId, bytes32 byteUri, bool safe) external;
+    function mintNonFungible(
+        address to,
+        uint256 tokenId,
+        bytes32 byteUri,
+        bool safe
+    ) external;
 }
 
 interface IREVV {
@@ -203,5 +256,9 @@ interface IREVV {
      *
      * Emits a {Transfer} event.
      */
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
 }
