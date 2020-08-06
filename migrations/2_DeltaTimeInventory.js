81c35dc84281ff5f6f9e931bf1f2ad40f116723b
Nathan Sala <sala.nathan@gmail.com>
2020-08-07 04:21:52 +0800

    add repair centre migration and improve migrations

--- migrations/2_DeltaTimeInventory.js
+++ migrations/2_DeltaTimeInventory.js
@@ -1,35 +1,20 @@
 const {MetaTxPayoutWallet} = require('../src/constants');
 
 const REVV = artifacts.require('REVV');
-const Address = artifacts.require('Address.sol');
-const SafeMath = artifacts.require('SafeMath.sol');
-const UInt256ToDecimalString = artifacts.require('UInt256ToDecimalString.sol');
-const Bytes32ToBase32String = artifacts.require('Bytes32ToBase32String.sol');
-const DeltaTimeInventory = artifacts.require('DeltaTimeInventoryV2.sol');
+const Bytes = artifacts.resolver.require('Bytes', '../imports');
+
+const DeltaTimeInventory = artifacts.resolver.require('DeltaTimeInventory', '../imports');
+DeltaTimeInventory.synchronization_timeout = 0;
+artifacts.cache.push(DeltaTimeInventory);
 
 module.exports = async (deployer, network, accounts) => {
-    await deployer.deploy(Address);
-    await Address.deployed();
-    const address = (await Address.deployed()).address;
-
-    await deployer.deploy(SafeMath);
-    await SafeMath.deployed();
-    const safeMath = (await SafeMath.deployed()).address;
-
-    await deployer.deploy(UInt256ToDecimalString);
-    await UInt256ToDecimalString.deployed();
-    const uInt256ToDecimalString = (await UInt256ToDecimalString.deployed()).address;
-
-    await deployer.deploy(Bytes32ToBase32String);
-    await Bytes32ToBase32String.deployed();
-    const bytes32ToBase32String = (await Bytes32ToBase32String.deployed()).address;
-
-    await DeltaTimeInventory.link('Address', address);
-    await DeltaTimeInventory.link('SafeMath', safeMath);
-    await DeltaTimeInventory.link('UInt256ToDecimalString', uInt256ToDecimalString);
-    await DeltaTimeInventory.link('Bytes32ToBase32String', bytes32ToBase32String);
-
     const revvContract = await REVV.deployed();
+
+    await deployer.deploy(Bytes);
+    await Bytes.deployed();
+    const bytes = (await Bytes.deployed()).address;
+
+    await DeltaTimeInventory.link('Bytes', bytes);
 
     await deployer.deploy(DeltaTimeInventory, revvContract.address, MetaTxPayoutWallet, {gas: 10000000});
     const inventoryContract = await DeltaTimeInventory.deployed();
