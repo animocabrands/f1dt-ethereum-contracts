df1e598f84decb0b8053b5cc5d525d4e0840b765
Vidhvat Madan <vidhvatm@animocabrands.com>
2020-09-30 16:27:47 +0800

    renames

--- migrations/6_TimeTrialEliteLeague.js
+++ migrations/6_TimeTrialEliteLeague.js
@@ -2,7 +2,7 @@ const {fromWei} = require('web3-utils');
 const {toBytes32Attribute} = require('@animoca/ethereum-contracts-assets_inventory').bytes32Attributes;
 
 const REVV = artifacts.require('REVV');
-const TimeTrialLeagues = artifacts.require('TimeTrialLeagues');
+const TimeTrialEliteLeague = artifacts.require('TimeTrialEliteLeague');
 
 const LeagueAmountsMap = {
     'A':10000,
@@ -15,13 +15,13 @@ const LockingPeriod = 1209600; // 14 Days
 module.exports = async (deployer, network, accounts) => {
     const revvContract = await REVV.deployed();
     await deployer.deploy(
-        TimeTrialLeagues,
+        TimeTrialEliteLeague,
         revvContract.address,
         LockingPeriod,
         Object.keys(LeagueAmountsMap).map(id=>toBytes32Attribute(id)),
         Object.values(LeagueAmountsMap)
     );
 
-    const timeTrialLeaguesContract = await TimeTrialLeagues.deployed();
-    console.log(timeTrialLeaguesContract);
+    const timeTrialEliteLeagueContract = await TimeTrialEliteLeague.deployed();
+    console.log(timeTrialEliteLeagueContract);
 };
