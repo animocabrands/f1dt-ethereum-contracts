3648f881658d70b4059309c459273cb4c1c26971
Jacky Yuk <jackyy@animocabrands.com>
2020-11-25 15:56:16 +0800

    WIP Prepaid: adding during sales test

--- test/game/PrePaid.test.js
+++ test/game/PrePaid.test.js
@@ -50,6 +50,7 @@ describe('PrePaid', function () {
             revvAmount.fill(toWei('1000000'));
             this.revv = await REVV.new(participants, revvAmount, {from: deployer});
             this.prepaid = await PrePaid.new(this.revv.address, {from: deployer});
+            this.prepaid.whitelistOperator( operator, true ,{from:deployer});
             // approval revv token to prepaid contract
             await this.revv.approve(this.prepaid.address, toWei('1000000'), {from: participant});
             await this.revv.approve(this.prepaid.address, toWei('1000000'), {from: participant2});
@@ -58,17 +59,35 @@ describe('PrePaid', function () {
         describe("setSaleState", function() {
 
             it("can set state BEFORE_SALE_STATE", async function() {
-                await expectEvent(await this.prepaid.setSaleState(0, {from: deployer}), "StateChange", {state : "0"});
+                await expectEvent(await this.prepaid.setSaleState('0', {from: deployer}), "StateChange", {state : "0"});
+                (await this.prepaid.state()).should.be.bignumber.equal("0");
+            });
+
+            it("can set state SALE_START_STATE", async function() {
+                await expectEvent(await this.prepaid.setSaleState('1', {from: deployer}), "StateChange", {state : "1"});
+                (await this.prepaid.state()).should.be.bignumber.equal("1");
+            });
+
+            it("can set state SALE_END_STATE", async function() {
+                await expectEvent(await this.prepaid.setSaleState('2', {from: deployer}), "StateChange", {state : "2"});
+                (await this.prepaid.state()).should.be.bignumber.equal("2");
+            });
+
+            it("revert if unknown state", async function() {
+                const revert = this.prepaid.setSaleState('3', {from: deployer});
+                await expectRevert(revert, 'PrePaid: invalid state');
             });
 
             it("revert if not owner", async function() {
                 const revert = this.prepaid.setSaleState(0, {from: anonymous});
                 await expectRevert(revert, "Ownable: caller is not the owner");
+                const revert2 = this.prepaid.setSaleState(0, {from: participant});
+                await expectRevert(revert2, "Ownable: caller is not the owner");
             });
 
         });
 
-        describe("unpaused", function() {
+        describe("unpaused beforeSales", function() {
             // Contract unpaused and beforeSales Starts
             beforeEach(async function() {
                 await this.prepaid.unpause({from: deployer});
@@ -108,8 +127,40 @@ describe('PrePaid', function () {
                 (await this.prepaid.balanceOf(participant2)).should.be.bignumber.equal(toWei('200'));
                 (await this.prepaid.globalDeposit()).should.be.bignumber.equal(toWei('300'));
             });
+
+            it("consume should revert", async function() {
+                await this.prepaid.deposit(toWei('100'), {from: participant});
+                expectRevert(this.prepaid.consume(participant, toWei('1'), {from: deployer}), "PrePaid: state locked");
+            });
+
+            it("withdraw should revert", async function() {
+                await this.prepaid.deposit(toWei('100'), {from: participant});
+                expectRevert(this.prepaid.withdraw({from: deployer}), "PrePaid: state locked");
+            });
         });
         
+        describe("during sales", function() {
+            beforeEach(async function() {
+                await this.prepaid.unpause({from: deployer});
+                await this.prepaid.deposit(toWei('1000'), {from: participant});
+                await this.prepaid.setSaleState('1', {from: deployer});
+            });
+
+            it.skip("consumes", async function() {
+
+            })
+
+            it("deposit should revert", async function(){
+                const revert = this.prepaid.deposit(toWei('1'), {from: participant});
+                await expectRevert(revert, "PrePaid: state locked");
+            });
+
+            it("withdraw should revert", async function() {
+                const revert = this.prepaid.withdraw({from: deployer});
+                expectRevert(revert, "PrePaid: state locked");
+            });
+
+        })
 
     })
 
