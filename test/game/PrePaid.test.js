3c7ec7d5e3a0358476a2a6dfe9d86c6d72a4b9d4
Jan-Paul Azucena <jp@fuelpowered.com>
2020-11-24 17:56:11 -0800

    [CHANGED] Little cleanup of the initial testing script for the PrePaid contract

--- test/game/PrePaid.test.js
+++ test/game/PrePaid.test.js
@@ -1,17 +1,28 @@
-const {accounts, contract} = require('@openzeppelin/test-environment');
-const {expect} = require('chai');
-const {expectEvent, expectRevert, time} = require('@openzeppelin/test-helpers');
-const {BN, fromAscii, toWei} = require('web3-utils');
-const {ZeroAddress} = require('@animoca/ethereum-contracts-core_library').constants;
+const { accounts, contract } = require('@openzeppelin/test-environment');
+const { expectRevert } = require('@openzeppelin/test-helpers');
+const { toWei } = require('web3-utils');
+const { ZeroAddress } = require('@animoca/ethereum-contracts-core_library').constants;
 
 const PrePaid = contract.fromArtifact('PrePaid');
 const REVV = contract.fromArtifact('REVV');
 
-const [deployer, participant] = accounts;
-
+const [ deployer, participant ] = accounts;
 
 describe('PrePaid', function () {
-    describe('constructor(revvContractAddress)', function () {
+
+    async function doDeploy(overrides = {}) {
+        this.revv = await REVV.new(
+            overrides.holders || [ participant ],
+            overrides.amounts || [ toWei('1000000') ],
+            { from: overrides.deployer || deployer });
+
+        this.contract = await PrePaid.new(
+            this.revv.address,
+            { from: overrides.deployer || deployer });
+    }
+
+    describe('constructor()', function () {
+
         it('should revert with a zero address for the revv contract', async function () {
             await expectRevert(
                 PrePaid.new(
@@ -21,15 +32,11 @@ describe('PrePaid', function () {
                 'PrePaid: zero address'
             );
         });
+
         it('should deploy with correct parameters', async function () {
-            this.revv = await REVV.new([participant], [toWei('1000000')], {from: deployer});
-            PrePaid.new(
-                this.revv.address,
-                1,
-                Object.keys(tiers).map((k) => fromAscii(k)),
-                Object.values(tiers).map((v) => toWei(v)),
-                {from: deployer}
-            );
+            await doDeploy.bind(this)();
         });
+
     });
+
 });
