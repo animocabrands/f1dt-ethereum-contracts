e3bd625e8642f34b5ea3d9da50bc15d47e0f54d7
Jacky Yuk <jackyy@animocabrands.com>
2020-11-25 11:23:52 +0800

    WIP: add deposit test from multiple participant

--- test/game/PrePaid.test.js
+++ test/game/PrePaid.test.js
@@ -6,7 +6,8 @@ const { ZeroAddress } = require('@animoca/ethereum-contracts-core_library').cons
 const PrePaid = contract.fromArtifact('PrePaid');
 const REVV = contract.fromArtifact('REVV');
 
-const [ deployer, participant, participant2 ] = accounts;
+const [ deployer, ...participants ] = accounts;
+const [participant, participant2] = participants;
 
 describe('PrePaid', function () {
 
@@ -46,30 +47,30 @@ describe('PrePaid', function () {
     describe("deposit(amount)", function() {
         
         beforeEach(async function() {
-            this.revv = await REVV.new([participant], [toWei('1000000')], {from: deployer});
+            const revvAmount = new Array(participants.length);
+            revvAmount.fill(toWei('1000000'));
+            this.revv = await REVV.new(participants, revvAmount, {from: deployer});
             this.prepaid = await PrePaid.new(this.revv.address, {from: deployer});
+            await this.prepaid.unpause({from: deployer});
             // approval revv token to prepaid contract
             await this.revv.approve(this.prepaid.address, toWei('1000000'), {from: participant});
+            await this.revv.approve(this.prepaid.address, toWei('1000000'), {from: participant2});
         });
 
         it("deposit(1revv)", async function(){
-            await this.prepaid.unpause({from: deployer});
             const receipt = await this.prepaid.deposit(toWei('1'), {from: participant});
-            const balance = await this.prepaid.balanceOf(participant);
-            balance.should.be.bignumber.equal(toWei('1'));
             await expectEvent(receipt, "Deposited", {
                 wallet: participant,
                 amount: toWei('1'),
             });
+            (await this.prepaid.balanceOf(participant)).should.be.bignumber.equal(toWei('1'));
+            (await this.prepaid.globalDeposit()).should.be.bignumber.equal(toWei('1'));
         });
 
         it("deposit(1revv) X 3", async function(){
-            await this.prepaid.unpause({from: deployer});
             const receipt = await this.prepaid.deposit(toWei('1'), {from: participant});
             const receipt1 = await this.prepaid.deposit(toWei('1'), {from: participant});
             const receipt2 = await this.prepaid.deposit(toWei('1'), {from: participant});
-            const balance = await this.prepaid.balanceOf(participant);
-            balance.should.be.bignumber.equal(toWei('3'));
             await expectEvent(receipt, "Deposited", {
                 wallet: participant,
                 amount: toWei('1'),
@@ -82,31 +83,49 @@ describe('PrePaid', function () {
                 wallet: participant,
                 amount: toWei('1'),
             });
+            (await this.prepaid.balanceOf(participant)).should.be.bignumber.equal(toWei('3'));
+            (await this.prepaid.globalDeposit()).should.be.bignumber.equal(toWei('3'));
         });
 
-        it.skip("deposit(1revv) from different participant with globalDeposit check", async function(){
-            
+        it("deposit(100revv) from different participant", async function(){
+            const receipt = await this.prepaid.deposit(toWei('100'), {from: participant});
+            const receipt1 = await this.prepaid.deposit(toWei('100'), {from: participant2});
+            const receipt2 = await this.prepaid.deposit(toWei('100'), {from: participant2});
+            await expectEvent(receipt, "Deposited", {
+                wallet: participant,
+                amount: toWei('100'),
+            });
+            await expectEvent(receipt1, "Deposited", {
+                wallet: participant2,
+                amount: toWei('100'),
+            });
+            await expectEvent(receipt2, "Deposited", {
+                wallet: participant2,
+                amount: toWei('100'),
+            });
+            (await this.prepaid.balanceOf(participant)).should.be.bignumber.equal(toWei('100'));
+            (await this.prepaid.balanceOf(participant2)).should.be.bignumber.equal(toWei('200'));
+            (await this.prepaid.globalDeposit()).should.be.bignumber.equal(toWei('300'));
         });
 
         it("should revert when paused", async function(){
+            await this.prepaid.pause({from: deployer});
             const promiseResult = this.prepaid.deposit(toWei('1'), {from: participant});
             await expectRevert(promiseResult,'Pausable: paused');
         })
 
         it("should revert if the salesStart", async function() {
-            await this.prepaid.unpause({from: deployer});
             await this.prepaid.setStartSale(true,{from: deployer});
             const promiseResult = this.prepaid.deposit(toWei('1'), {from: participant});
             await expectRevert(promiseResult,'PrePaid: sale started');
         });
 
         it("should revert if the salesEnd", async function() {
-            await this.prepaid.unpause({from: deployer});
             await this.prepaid.setEndSale(true,{from: deployer});
             const promiseResult = this.prepaid.deposit(toWei('1'), {from: participant});
             await expectRevert(promiseResult,'PrePaid: sale ended');
         });
 
-    })
+    });
 
 });
