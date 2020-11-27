2a7d71843b96a0a86ed0cddf8f34d54fc610b008
Sergio Miguel <sergiobmiguel@msn.com>
2020-11-27 11:36:43 -0300

    WIP: F1DT tokens for crate

---
+++ contracts/token/ERC20/F1DT_LCK.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.8;
+
+import "./ERC20WithFixedSupply.sol";
+
+/**
+ * @title F1 Delta Time Legendary Crate Key
+ */
+contract F1DT_LCK is ERC20WithFixedSupply {
+    // solhint-disable-next-line const-name-snakecase
+    string public constant override symbol = "F1DT.LCK";
+    // solhint-disable-next-line const-name-snakecase
+    string public constant override name = "F1&#174; Delta Time Legendary Crate Key";
+    // solhint-disable-next-line const-name-snakecase
+    uint8 public constant override decimals = 18;
+
+    constructor(address[] memory holders, uint256[] memory amounts, uint256 totalSupply) public ERC20WithFixedSupply(totalSupply) {
+        require(holders.length == amounts.length, "F1DT_LCK: wrong arguments");
+        for (uint256 i = 0; i < holders.length; ++i) {
+            _mint(holders[i], amounts[i]);
+        }
+    }
+}
